b1bb443a9c429e89ee77c3a2a392c3e5554b95ba
przemek <przemyslaw.soltan@gmail.com>
2021-04-12 18:19:37 +0200

    fix(ubg) - red (2)

--- src/ont2red.js
+++ src/ont2red.js
@@ -4,20 +4,70 @@ const fs = require('fs')
 let out = []
 
 let data = fs.readFileSync(path.join(__dirname, '../dist/pubg.ont'), 'utf8').toString()
-
 if (data.charCodeAt(0) === 0xFEFF) {
     data = data.slice(1)
 }
-
 data = data.split(/\n/)
 
 console.log('[UBG]', data.length)
 
+let input = fs.readFileSync(path.join(__dirname, '../src/arch/ubg-old.txt'), 'utf8').toString().split(/\n/)
+console.log('[UBG old', input.length)
+
 // czytamy kolejne wiersze
+
+let idx = 0
 
 data.forEach((item, i) => {
     let row = data[i]
-    out.push(row)
+    let detect = input[i]
+    if (detect === undefined) {
+        out.push(row)
+    } else {
+        let split = detect.split(/<[/]?J>/g);
+        if (split.length > 1) {
+            let line = []
+
+            // przypadek 1
+
+            if (split.length === 3) {
+                if (split[0].length === 0 && split[2].length === 0) {
+                    line.push('<J>')
+                    line.push(row)
+                    line.push('</J>')
+                } else {
+                    // przypadek 2
+                    // pierwsze słowo po <J>
+                    let split2 = split[1].split(' ');
+                    let word = split2[0]
+                    let searchIndex = row.search(word);
+                    line.push(row.substring(0, searchIndex))
+                    line.push('<J>')
+
+                    let row2 = row.substring(searchIndex)
+                    if ( split[2].length === 0) {
+                        line.push(row2)
+                    }
+                    else {
+                        // ostatnie słowo przd </J>
+                        let lastWord = split2[split2.length - 1]
+                        let searchIndex2 = row2.search(lastWord);
+                        line.push(row2.substring(0, searchIndex2 + lastWord.length))
+                        line.push('</J>')
+                        line.push(row2.substring(searchIndex2 + lastWord.length))
+                    }
+                }
+                out.push(line.join(''))
+            }
+            else {
+                console.log(i, row)
+                out.push('????')
+            }
+
+        } else {
+            out.push(row)
+        }
+    }
 })
 
 fs.writeFileSync('./dist/pubg-red.ont',  '\ufeff' + out.join('\n'), {encoding: 'utf-8'})
